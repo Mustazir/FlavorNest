f1d0e44ac250f1ea3e1835f320be79dfe3beef69
Mustazir Billah <billah15-5193@diu.edu.bd>
2025-02-16 22:52:33 +0600

    error fix Recommend component

--- src/Routs/pages/Components/Recommends.jsx
+++ src/Routs/pages/Components/Recommends.jsx
@@ -1,31 +1,31 @@
-// import Card from "../../../Shared/Card";
-// import Title from "../../../Shared/title";
-// import useMenu from "../../../Shared/useMenu";
+import Card from "../../../Shared/Card";
+import Title from "../../../Shared/title";
+import useMenu from "../../../Shared/useMenu";
 
-// const Recommends = () => {
-//   const [menu] = useMenu();
-//   const recommend = menu.filter((item) => item.category === "offered").slice(0, 3);
+const Recommends = () => {
+  const [menu] = useMenu();
+  const recommend = menu.filter((item) => item.category === "offered").slice(0, 3);
 
-//   return (
-//     <>
-//       <div>
-//         <Title heading="CHEF RECOMMENDS" subheading="Should Try" />
-//       </div>
+  return (
+    <>
+      <div>
+        <Title heading="CHEF RECOMMENDS" subheading="Should Try" />
+      </div>
 
      
 
-//       <div className="md:flex gap-6 mx-auto p-5  ">
-//         {recommend.map((item) => (
-//           <Card
-//             key={item._id}
-//             name={item.name}
-//             description={item.recipe}
-//             img={item.image}
-//           />
-//         ))}
-//       </div>
-//     </>
-//   );
-// };
+      <div className="md:flex gap-6 mx-auto p-5  ">
+        {recommend.map((item) => (
+          <Card
+            key={item._id}
+            name={item.name}
+            description={item.recipe}
+            img={item.image}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
-// export default Recommends;
+export default Recommends;
